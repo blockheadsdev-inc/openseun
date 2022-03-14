90418d18916931f3e582c27a91948a6ab87ace37
KahigaKiguru <dev.enzitechnologies@gmail.com>
2022-03-14 17:28:59 +0300

    initial commit

---
+++ src/test/java/io/seunwater/openseun/OpenseunApplicationTests.java
@@ -0,0 +1,13 @@
+package io.seunwater.openseun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenseunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
